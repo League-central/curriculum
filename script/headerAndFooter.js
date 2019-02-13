1400582a7ff4010433ae8bb7c81f750985ffdec1
mjfre <mjfre@aol.com>
2019-02-12 17:08:16 -0800

    update header and footer script
    please don't break everything

--- script/headerAndFooter.js
+++ script/headerAndFooter.js
@@ -3,13 +3,14 @@ function addHeader() {
 
     links = document.getElementById("header");
 
-    links.innerHTML += "<span>";
-    links.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/img/league.jpg\" alt=\"league-logo\"></a>";
+    links.innerHTML += "<span style=\"width:100%\">";
+    links.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"img/league.jpg\" alt=\"league-logo\" title=\"League Website\"></a>";
     links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
-    links.innerHTML += "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/img/teacher.png\" alt=\"Teacher Resources\"</a>";
+    links.innerHTML += "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"img/teacher.png\" alt=\"Teacher Resources\" title=\"Leauge Resources\"></a>";
     links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
-    links.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/img/pike13.png\" alt=\"Legaue Hub\"></a>";
-    links.innerHTML += "<a href=\"mailto:curriculum@jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/img/suggestions.jpg\" alt=\"Recipe Suggestions\"</a>";  
+    links.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"img/pike13.png\" alt=\"Legaue Hub\" title=\"League Hub\"></a>";
+    links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
+    links.innerHTML += "<a href=\"mailto:curriculum@jointheleague.org\"><img class=\"footer\" src=\"img/suggestions.png\" alt=\"Recipe Suggestions\"title=\"Suggestions\"></a>";  
     links.innerHTML += "</span>";
 }
 
@@ -19,15 +20,13 @@ function addFooter() {
 
     footer.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/img/league.jpg\" alt=\"league-logo\" align=\"center\"></a>";
 
-    footer.innerHTML += "<span style=\"position:absolute;bottom:20px;\">Copyright <a href=\"http://www.jointheleague.org\">The League of Amazing Programmers</a> 2013-2019</span>";
+    footer.innerHTML += "<span style=\"position:absolute;bottom:20px;font-weight: bold;\">Copyright <a href=\"http://www.jointheleague.org\">The League of Amazing Programmers</a> 2013-2018</span>";
 
     //footer.innerHTML += "References:";
 
     //footer.innerHTML += "<a href=\"https://docs.oracle.com/javase/8/docs/api/\"><img class=\"footer\" src=\"https://league-level0.github.io/java.png\" alt=\"Java API\"  align=\"center\"</a>";
 
     //footer.innerHTML += "<a href=\"https://processing.org/reference\"><img class=\"footer\" src=\"https://league-level0.github.io/p3logo.jpeg\" alt=\"Processing Reference\"  align=\"center\"></a>";
-
-
 
 }
 
@@ -40,10 +39,26 @@ function addRecipeHeader(){
     
     links.innerHTML += "<span>";
      links.innerHTML += "<a style='font-size:3em;color: #fa591a;text-decoration: none;cursor: pointer;padding:.5em;' onclick=' window.history.back();'>&#171;</a>"
-
-    links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
+    
 }
 
-function goBack() {
-   
+function addRecipeFooter(){
+    var footer = document.getElementById("footer");
+
+    footer.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/img/league.jpg\" alt=\"league-logo\" align=\"center\"></a>";
+
+    footer.innerHTML += "<span style=\"position:absolute;bottom:20px;font-weight: bold;\">Copyright <a href=\"http://www.jointheleague.org\">The League of Amazing Programmers</a> 2013-2019</span>";
+
+    //footer.innerHTML += "References:";
+
+    //footer.innerHTML += "<a href=\"https://docs.oracle.com/javase/8/docs/api/\"><img class=\"footer\" src=\"https://league-level0.github.io/java.png\" alt=\"Java API\"  align=\"center\"</a>";
+
+    //footer.innerHTML += "<a href=\"https://processing.org/reference\"><img class=\"footer\" src=\"https://league-level0.github.io/p3logo.jpeg\" alt=\"Processing Reference\"  align=\"center\"></a>";
 }
+
+function copyDivToClipboard() {
+            var range = document.getSelection().getRangeAt(0);
+            range.selectNode(document.getElementByClassName("code")[0]);
+            window.getSelection().addRange(range);
+            document.execCommand("copy")
+        }

d8f82fad0e382beb7bda7ce918546ffbd17036cb
mjfre <mjfre@aol.com>
2021-02-02 22:06:13 -0800

    Updating index.html

--- levels/Level7/resources/3_milestone/HomeController.java
+++ levels/Level7/resources/3_milestone/HomeController.java
@@ -2,7 +2,6 @@ package org.jointheleague.level7.cheetah.presentation;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
--- levels/Level7/resources/4_milestone/LocController.java
+++ levels/Level7/resources/4_milestone/LocController.java
@@ -3,18 +3,9 @@ package org.jointheleague.level7.cheetah.presentation;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.jointheleague.level7.cheetah.domain.Result;
-import org.jointheleague.level7.cheetah.service.LocService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 public class LocController {
@@ -27,7 +18,7 @@ public class LocController {
             @ApiResponse(code = 200, message = "Result(s) found"),
             @ApiResponse(code = 404, message = "Result(s) not found")
     })
-    public List<Result> getResults(@RequestParam(value="q") String query){
+    public String getResults(@RequestParam(value="q") String query){
         return "Searching for books related to " + query;
     }
 
